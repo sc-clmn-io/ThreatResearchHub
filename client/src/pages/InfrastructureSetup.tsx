034ae24717a56f8682d3481613c064277e4088b7
ThreatResearchHub <security-research-platform@replit.com>
2025-08-01 10:50:43 -0400

    ThreatResearchHub auto-backup - 8/1/2025

--- client/src/pages/InfrastructureSetup.tsx
+++
@@ -1,9 +0,0 @@
-import { InfrastructureQuickAccess } from '@/components/infrastructure-quick-access';
-
-export function InfrastructureSetup() {
-  return (
-    <div className="container mx-auto py-8">
-      <InfrastructureQuickAccess />
-    </div>
-  );
-}
--- client/src/pages/github-export.tsx
+++
@@ -1,5 +0,0 @@
-import GitHubExport from '@/components/github-export';
-
-export default function GitHubExportPage() {
-  return <GitHubExport />;
-}
